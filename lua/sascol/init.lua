c0d08b535178b1e49c87b90b982eea0c7c9a6e65
Wasp-mk <gradientcolors.mk@gmail.com>
2026-04-14 20:08:45 -0300

    add colors

--- lua/sascol/init.lua
+++ lua/sascol/init.lua
@@ -12,141 +12,145 @@ function M.load()
 
   local groups = {
     -- UI Groups
-    ColorColumn = { bg = p.main.grey },
+    ColorColumn = { bg = p.extra.grey3 },
 
-    Conceal = { fg = p.main.lightGrey },
+    CursorColumn = { bg = p.extra.grey3 },
+    CursorLine = { bg = p.extra.grey3 },
 
-    CursorColumn = { bg = p.main.grey },
-    CursorLine = { bg = p.main.grey },
-    CursorLineNr = { fg = p.main.yellow },
+    Conceal = { fg = p.main.grey1 },
 
-    DiagnosticError = { fg = p.main.red },
-    DiagnosticHint = { fg = p.main.lightBlue },
-    DiagnosticInfo = { fg = p.main.lightYellow },
-    DiagnosticOk = { fg = p.main.lightGreen },
-    DiagnosticWarn = { fg = p.main.yellow },
+    DiagnosticError = { fg = p.main.red1 },
+    DiagnosticHint = { fg = p.main.cyan2 },
+    DiagnosticInfo = { fg = p.main.yellow2 },
+    DiagnosticOk = { fg = p.main.green2 },
+    DiagnosticWarn = { fg = p.main.yellow1 },
 
-    DiffAdd = { fg = p.main.black, bg = p.main.lightGreen },
-    DiffChange = { fg = p.main.black, bg = p.main.lightMagenta},
-    DiffDelete = { fg = p.main.black, bg = p.main.lightRed },
-    DiffText = { fg = p.main.black, bg = p.main.lightGrey },
+    DiffAdd = { fg = p.main.black, bg = p.main.green2 },
+    DiffChange = { fg = p.main.black, bg = p.main.magenta2},
+    DiffDelete = { fg = p.main.black, bg = p.main.red2 },
+    DiffText = { fg = p.main.black, bg = p.main.blue2 },
 
-    Directory = { fg = p.main.lightBlue },
+    Directory = { fg = p.main.blue2 },
 
-    ErrorMsg = { fg = p.main.red },
-    ModeMsg = { fg = p.main.lightYellow },
-    MoreMsg = { fg = p.main.blue },
-    OkMsg = { fg = p.main.lightGreen },
-    WarningMsg = { fg = p.main.yellow },
+    OkMsg = { fg = p.main.green2, bg = p.main.black },
+    WarningMsg = { fg = p.main.yellow1, bg = p.main.black },
+    ErrorMsg = { fg = p.main.red1, bg = p.main.black },
+    StderrMsg = { fg = p.main.red1, bg = p.main.black },
+    StdoutMsg = { fg = p.main.grey2, bg = p.main.black },
+    ModeMsg = { fg = p.main.yellow2, bg = p.main.black },
+    MsgArea = { fg = p.main.grey2, bg = p.main.black },
+    MsgSeparator = { fg = p.main.white, bg = p.main.black },
+    MoreMsg = { fg = p.main.blue1, bg = p.main.black },
 
     TermCursor = { fg = p.main.black, bg = p.main.white},
 
-    Folded = { fg = p.main.lightGrey, bg = p.main.none },
-    FoldColumn = { fg = p.main.lightGrey, bg = p.main.none },
+    Folded = { fg = p.main.grey2, bg = 'none' },
+    FoldColumn = { fg = p.main.grey2, bg = 'none' },
 
-    LineNr = { fg = p.main.lightGrey },
-    LineNrBelow = { fg = p.main.lightGrey },
-    SignColumn = { fg = p.main.none, bg = p.main.none },
+    CursorLineNr = { fg = p.main.yellow1 },
+    LineNr = { fg = p.extra.grey4 },
+    LineNrBelow = { fg = p.extra.grey4 },
+    SignColumn = { fg = 'none', bg = 'none' },
 
-    MatchParen = { fg = p.main.yellow, bg = p.main.magenta },
+    MatchParen = { fg = p.main.yellow1, bg = p.main.magenta1 },
 
-    NonText = { fg = p.main.lightGrey },
-    Normal = { fg = p.main.lightGrey, bg = p.main.none },
-    NormalFloat = { fg = p.main.lightGrey, bg = p.main.black },
+    NonText = { fg = p.main.grey2 },
+    Normal = { fg = p.main.grey2, bg = 'none' },
+    NormalFloat = { fg = p.main.grey2, bg = p.main.black },
 
-    Pmenu = { fg = p.main.lightGrey, bg = p.main.black },
-    PmenuSel = { fg = p.main.black, bg = p.main.lightGrey },
+    Pmenu = { fg = p.main.grey2, bg = p.main.black },
+    PmenuSel = { fg = p.main.black, bg = p.main.grey2 },
     PmenuMatch = { fg = p.main.white, bg = p.main.black },
     PmenuMatchSel = { fg = p.main.white, bg = p.main.black },
-    PmenuSbar = { fg = p.main.lightGrey, bg = p.main.black },--scrollbar
-    PmenuThumb = { bg = p.main.lightGrey },--scrollBar
-    WildMenu = { fg = p.main.lightGrey, bg = p.main.black },
+    PmenuSbar = { fg = p.main.grey2, bg = p.main.black },--scrollbar
+    PmenuThumb = { bg = p.main.grey2 },--scrollBar
+    WildMenu = { fg = p.main.grey2, bg = p.main.black },
 
-    IncSearch = { fg = p.main.black, bg = p.main.magenta },
-    Substitute = { fg = p.main.black, bg = p.main.lightYellow },
-    Search = { fg = p.main.black, bg = p.main.yellow },
-    CurSearch = { fg = p.main.black, bg = p.main.magenta },
+    IncSearch = { fg = p.main.black, bg = p.main.magenta1 },
+    Substitute = { fg = p.main.black, bg = p.main.yellow2 },
+    Search = { fg = p.main.black, bg = p.main.yellow1 },
+    CurSearch = { fg = p.main.black, bg = p.main.magenta1 },
 
-    SpellBad = { undercurl = true, sp = p.main.lightRed },
-    SpellCap = { undercurl = true, sp = p.main.lightYellow },
-    SpellLocal = { undercurl = true, sp = p.main.lightGreen },
-    SpellRare = { undercurl = true, sp = p.main.blue },
+    SpellBad = { undercurl = true, sp = p.main.red2 },
+    SpellCap = { undercurl = true, sp = p.main.yellow2 },
+    SpellLocal = { undercurl = true, sp = p.main.green2 },
+    SpellRare = { undercurl = true, sp = p.main.blue1 },
 
-    Visual = { fg = p.main.none, bg = p.main.grey },
-    whitespace = { fg = p.main.lightGrey },
+    Visual = { fg = 'none', bg = p.extra.grey3 },
+    whitespace = { fg = p.main.grey2 },
 
     StatusLine = { fg = p.main.white, bg = p.main.black },
-    StatusLineNC = { fg = p.main.lightgrey, bg = p.main.black, },
+    StatusLineNC = { fg = p.main.grey2, bg = p.main.black, },
     StatusLineTerm = { fg = p.main.white, bg = p.main.black },
-    StatusLineTermNC = { fg = p.main.lightGrey, bg = p.main.black },
+    StatusLineTermNC = { fg = p.main.grey2, bg = p.main.black },
 
     WinBar = { fg = p.main.white, bg = p.main.black },
-    WinBarNC = { fg = p.main.lightGrey, bg = p.main.black },
+    WinBarNC = { fg = p.main.grey2, bg = p.main.black },
     WinSeparator = { fg = p.main.white },
 
     TabLine = { fg = p.main.white, bg = p.main.black },
     TabLineSel = { fg = p.main.black, bg = p.main.white },
-    TabLineFill = { fg = p.main.white, bg = p.main.none },
+    TabLineFill = { fg = p.main.white, bg = 'none' },
 
     -- Syntax Groups
-    Comment = { fg = p.main.lightGrey, italic = false }, --any comment
+    Comment = { fg = p.extra.grey4, italic = false }, --any comment
 
-    Constant = { fg = p.main.blue }, --any constant
-    String = { fg = p.main.lightGreen }, --a string constant: "this is a string"
-    Character = { fg = p.main.lightGreen }, --a character constant: 'c', '\n'
-    Number = { fg = p.main. yellow}, --a number constant: 234, 0xff
-    Boolean = { fg = p.main.blue },--a boolean constant: TRUE, false
-    Float = { fg = p.main.yellow },--a floating point constant: 2.3e10
+    Constant = { fg = p.main.blue1 }, --any constant
+    String = { fg = p.main.green2 }, --a string constant: "this is a string"
+    Character = { fg = p.main.green2 }, --a character constant: 'c', '\n'
+    Number = { fg = p.main.yellow1}, --a number constant: 234, 0xff
+    Boolean = { fg = p.main.blue1 },--a boolean constant: TRUE, false
+    Float = { fg = p.main.yellow1 },--a floating point constant: 2.3e10
 
-    Identifier = { fg = p.main.green },--any variable name
-    Function = { fg = p.main.lightMagenta },--function name (also: methods for classes)
+    Identifier = { fg = p.main.green1 },--any variable name
+    Function = { fg = p.main.magenta2 },--function name (also: methods for classes)
 
-    Statement = { fg = p.main.lightBlue },--any statement
-    Conditional = { fg = p.main.red },--if, then, else, endif, switch, etc.
-    Repeat = { fg = p.main.green },--for, do, while, etc.
-    Label = { fg = p.main.pink },--case, default, etc.
-    Operator = { fg = p.main.orange },--"sizeof", "+", "*", etc.
-    Keyword = { fg = p.main.lightRed, bold = false },--any other keyword
-    Exception = { fg = p.main.orange },--try, catch, throw
+    Statement = { fg = p.main.blue2 },--any statement
+    Conditional = { fg = p.main.red1 },--if, then, else, endif, switch, etc.
+    Repeat = { fg = p.main.green1 },--for, do, while, etc.
+    Label = { fg = p.extra.pink1 },--case, default, etc.
+    Operator = { fg = p.extra.orange1 },--"sizeof", "+", "*", etc.
+    Keyword = { fg = p.main.red2, bold = false },--any other keyword
+    Exception = { fg = p.extra.orange2 },--try, catch, throw
 
-    PreProc = { fg = p.main.lightBlue },--generic Preprocessor
-    Include = { fg = p.main.lightRed },--preprocessor #include
-    Define = { fg = p.main.lightBlue },--preprocessor #define
-    Macro = { fg = p.main.pink },--same as Define
-    PreCondit = { fg = p.main.lightBlue },--preprocessor #if, #else, #endif, etc.
+    PreProc = { fg = p.main.blue2 },--generic Preprocessor
+    Include = { fg = p.main.red2 },--preprocessor #include
+    Define = { fg = p.main.blue2 },--preprocessor #define
+    Macro = { fg = p.extra.pink1 },--same as Define
+    PreCondit = { fg = p.main.blue2 },--preprocessor #if, #else, #endif, etc.
 
-    Type = { fg = p.main.lightBlue },--int, long, char, etc.
-    StorageClass = { fg = p.main.lightBlue },--static, register, volatile, etc.
-    Structure = { fg = p.main.pink },--struct, union, enum, etc.
-    Typedef = { fg = p.main.blue },--a typedef
+    Type = { fg = p.main.blue2 },--int, long, char, etc.
+    StorageClass = { fg = p.main.blue2 },--static, register, volatile, etc.
+    Structure = { fg = p.extra.pink1 },--struct, union, enum, etc.
+    Typedef = { fg = p.main.blue1 },--a typedef
 
-    Special = { fg = p.main.lightBlue },--any special symbol
-    SpecialChar = { fg = p.main.lightRed },--special character in a constant
-    Tag = { fg = p.main.red },--you can use CTRL-] on this
-    Delimiter = { fg = p.main.orange },--character that needs attention
-    SpecialComment = { fg = p.main.lightYellow },--special things inside a comment
-    Debug = { fg = p.main.lightYellow },--debugging statements
+    Special = { fg = p.main.blue2 },--any special symbol
+    SpecialChar = { fg = p.main.red2 },--special character in a constant
+    Tag = { fg = p.main.red1 },--you can use CTRL-] on this
+    Delimiter = { fg = p.extra.orange1 },--character that needs attention
+    SpecialComment = { fg = p.main.yellow2 },--special things inside a comment
+    Debug = { fg = p.main.yellow2 },--debugging statements
 
-    Underline = { fg = p.main.lightRed },--text that stands out, HTML links
-    Ignore = { fg = p.main.grey },--left blank, hidden  hl-Ignore
-    Error = { fg = p.main.red },--any erroneous construct
-    Todo = { fg = p.main.black, bg = p.main.lightBlue },--anything that needs extra attention; mostly the keyword TODO FIXME and XXX
+    Underline = { fg = p.main.red2 },--text that stands out, HTML links
+    Ignore = { fg = p.main.grey1 },--left blank, hidden  hl-Ignore
+    Error = { fg = p.main.red1 },--any erroneous construct
+    Todo = { fg = p.main.black, bg = p.main.blue2 },--anything that needs extra attention; mostly the keyword TODO FIXME and XXX
 
-    Added = { fg = p.main.lightGreen },--added line in a diff
-    Changed = { fg = p.main.lightYellow },--changed line in a diff
-    Removed = { fg = p.main.lightRed },--removed line in a diff
+    Added = { fg = p.main.green2 },--added line in a diff
+    Changed = { fg = p.main.yellow2 },--changed line in a diff
+    Removed = { fg = p.main.red2 },--removed line in a diff
 
     -- treesitter
     ["@variable"] = { fg = p.main.white },
-    ["@variable.parameter"] = { fg = p.main.lightYellow },
-    ["@variable.member"] = { fg = p.main.green },
+    ["@variable.parameter"] = { fg = p.main.yellow2 },
+    ["@variable.member"] = { fg = p.main.green1 },
 
-    ["@keyword.type"] = { fg = p.main.pink },--keywords describing namespaces and composite types (e.g. `struct`, `enum`)
-    ["@keyword.modifier"] = { fg = p.main.pink },--keywords modifying other constructs (e.g. `const`, `static`, `public`)
-    ["@keyword.repeat"] = { fg = p.main.green },--keywords related to loops (e.g. `for`, `while`)
-    ["@keyword.return"] = { fg = p.main.pink }, --keywords like `return` and `yield`
+    ["@keyword.type"] = { fg = p.extra.pink1 },--keywords describing namespaces and composite types (e.g. `struct`, `enum`)
+    ["@keyword.modifier"] = { fg = p.extra.pink2 },--keywords modifying other constructs (e.g. `const`, `static`, `public`)
+    ["@keyword.repeat"] = { fg = p.main.green1 },--keywords related to loops (e.g. `for`, `while`)
+    ["@keyword.return"] = { fg = p.extra.pink1 }, --keywords like `return` and `yield`
 
-    ["@punctuation.bracket"] = { fg = p.main.lightGrey }, --brackets (e.g. `()`, `{}`, `[]`)
+    ["@punctuation.bracket"] = { fg = p.main.grey2 }, --brackets (e.g. `()`, `{}`, `[]`)
   }
 
   for group, settings in pairs(groups) do
